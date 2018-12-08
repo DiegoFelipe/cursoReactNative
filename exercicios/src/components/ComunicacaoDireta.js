38b5b0c70331ecce9b64a95942ac808ad551aa4e
Diego Felipe <diegofelipe.14@gmail.com>
2018-12-08 09:05:55 -0400

    adding new component

---
+++ exercicios/src/components/ComunicacaoDireta.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const fonte = { style : {fontSize: 40} }
+
+const filhosComProps = props => React.children.map(c => React.cloneElement(c, {...props, c.props}))
+
+export const Filho = props => 
+    <>
+     <Text {...fonte}>Filho: {props.nome} {props.sobrenome} </Text>
+    </>
+
+export const Pai = props =>
+    <>
+        
+    </>
